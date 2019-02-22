02f0778a90a82637682d75db50a7175d029019b6
jstntrn <justin.tran2290@gmail.com>
2019-02-22 13:58:38 -0700

    s3 setup

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 require('dotenv').config();
 const massive = require('massive');
-const { SERVER_PORT, DB_CONNECTION, SESSION_SECRET } = process.env;
+const { SERVER_PORT, DB_CONNECTION, SESSION_SECRET, S3_BUCKET, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY } = process.env;
 const ac = require('./controllers/authController');
 const cc = require('./controllers/contentController');
 const pc = require('./controllers/projController');
 const sc = require('./controllers/slideController');
 const vc = require('./controllers/vidController');
 const session = require('express-session');
+const aws = require('aws-sdk');
 
 const app = express();
 app.use(express.json())
@@ -50,3 +51,44 @@ app.post('/api/slide/:project', sc.createSlide)
 app.post('/api/content/:slide', cc.createContent)
 app.get('/api/content/:slide', cc.getSlideContent)
 app.delete('/api/content/:id', cc.deleteContent)
+
+//aws
+//just a simple get endpoint, make sure it matches what you have on your front end. We'll write out the function here instead of putting it inside of a controller. 
+app.get('/sign-s3', (req, res) => {
+
+    //set the config object that we're going to send - make sure your region matches the region code you specified when you created your bucket, and then put your access key and secret access key on the object as well
+   
+     aws.config = {
+       region: 'us-west-1',
+       accessKeyId: AWS_ACCESS_KEY_ID,
+       secretAccessKey: AWS_SECRET_ACCESS_KEY
+     }
+     
+     // we're using a function invoked from aws, grabbing our filename and filetype
+     const s3 = new aws.S3();
+     const fileName = req.query['file-name'];
+     const fileType = req.query['file-type'];
+     // and using these as a s3Params object that we are going to send to amazon (that function is accessible on the new s3 object instance we created)
+     // add your bucket name, add the key as the fileName, how long the request will be active for (in seconds), filetype, and ACL as 'public-read'
+     const s3Params = {
+       Bucket: S3_BUCKET,
+       Key: fileName,
+       Expires: 60,
+       ContentType: fileType,
+       ACL: 'public-read'
+     };
+    // now that we have our params configured, lets call the getSignedURL function (also lives on the s3 instance) tell it to 'putObject', provide the params, and as soon as that's run, it will execute a callback function we provide it.
+     s3.getSignedUrl('putObject', s3Params, (err, data) => {
+       if(err){
+         console.log(err);
+         return res.end();
+       }
+       // We can build our own URL here, as this is the format of the URL. Send back data if nothing erred. This data is Amazons go ahead to post something. We'll create an object called returnData, and then send that back to the front end! 
+       const returnData = {
+         signedRequest: data,
+         url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+       };
+   
+       return res.send(returnData)
+     });
+   });
--- src/components/ContentModal/ContentModal.js
+++ src/components/ContentModal/ContentModal.js
@@ -9,7 +9,8 @@ export default class ContentModal extends Component{
         this.state = {
             type: 'text',
             title: '',
-            content: ''
+            content: '',
+            url: ''
         }
     }
 
@@ -39,6 +40,55 @@ export default class ContentModal extends Component{
         .catch(err => console.log(err))
     }
 
+    uploadFile = (signedRequest, file, url) => {
+        // here we're just setting the header, as defined in the docs, to tell amazon what type of file we're going to have
+         const options = {
+           headers: {
+             'Content-Type': file.type,
+           },
+         };
+         //and we simply make our request.
+         axios
+         .put(signedRequest, file, options)
+         .then(response => {
+           this.setState({ url });
+           // Here we can do anything with the URL, setting state isn't required - but you may want to put this URL in your database.
+         })
+         .catch(err => {
+           if (err.response.status === 403) {
+             alert(
+               `Your request for a signed URL failed with a status 403. Double check the CORS configuration and bucket policy in the README. You also will want to double check your AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY in your .env and ensure that they are the same as the ones that you created in the IAM dashboard. You may need to generate new keys\n${
+                 err.stack
+               }`
+             );
+           } else {
+             alert(`ERROR: ${err.status}\n ${err.stack}`);
+           }
+         });
+     };
+
+    getSignedRequest = (e) => {
+        let file = e.target.files[0];
+        // here, we are getting the entire file, and it's properties. just like e.target.value is returning us some text on a input text onChange, we'll get an array of files that were uploaded. Since we're only uploading one - we will just pull it by referencing e.target.files[0]
+        
+        axios.get('/sign-s3', { //this is our own endpoint we will set up, and this is just a fancy way to send params. as long as you get the filename and type through, you can do it however you like.
+        params: {
+            'file-name': file.name,
+            'file-type': file.type
+        }
+        }).then( (response) => {
+        // like i said earlier, we are grabbing a "signed request" and a url, from amazon that is allowing us to actually upload the file. The url is where the file WILL be stored - but it hasn't been yet. 
+        //if we successfully get a response and enter the .then, then we will call the uploadFile function (described later ) with our 
+        // 1) signedRequest, 2) file (taken from above), and 3) url where the image will go 
+        const { signedRequest, url } = response.data 
+        this.uploadFile(signedRequest, file, url)
+
+        }).catch( err => {
+        // just catches the error of something went wrong on the server end
+        console.log(err)
+        })
+    }
+
     render(){
         const { title, content } = this.state
         return(
@@ -57,6 +107,10 @@ export default class ContentModal extends Component{
                         <input className='modal-title' value={title} placeholder={'title'} onChange={(e) => this.handleChange('title', e.target.value)} />
                     </div>
                     <textarea className='modal-content' value={content} placeholder={'content'} onChange={(e) => this.handleChange('content', e.target.value)} />
+                    <div className='modal-upload-wrapper'>
+                        <input className='modal-upload' type='file' name='file-upload' onChange={this.getSignedRequest}></input>
+                        <img src={this.state.url} alt='uploaded-image' />
+                    </div>
                 </div>
                 <button className='modal-button create' onClick={() => this.handleCreate()}><i className="fas fa-check"></i></button>
             </div>
