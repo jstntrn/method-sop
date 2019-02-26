6f6c9a774b16587cd09ab043fa577d18fe36933b
jstntrn <justin.tran2290@gmail.com>
2019-02-26 13:39:11 -0700

    sendgrid email html written and tested

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -5,6 +5,8 @@ import { updateUser } from './../../ducks/reducer'
 import { connect } from 'react-redux';
 import './Account.scss'
 
+
+
 class Account extends Component{
     constructor(props){
         super(props);
@@ -13,8 +15,8 @@ class Account extends Component{
                 to: 'justin.tran2290@gmail.com',
                 from: 'justin@printloon.com',
                 subject: 'Test Email from Method',
-                text: 'This is plain text version of the email, will show if html not available',
-                html: '<h1>This is the html version of the email</h1>'
+                text: 'Method is a video-based procedure implementation platform. With Method, you and your colleagues can easily create, share, and implement standard operating procedures better than ever before.',
+                html: `<div><header><h1 style="text-align: center;">Method</h1></header><div><div style="color: black;"><img src="https://methodsop-0001.s3.amazonaws.com/macbook.png" style="display:block, align-self: center;" width="200px"/><h3>Bob Ross invited you to join Method</h3><p>Method is a video-based procedure implementation platform. With Method, you and your colleagues can easily create, share, and implement standard operating procedures better than ever before.</p><a href="http://www.google.com/" target="_blank"><h2>Create Account</h2></a></div></div><div><p>Copyright © 2019 Method, All rights reserved.</p></div></div>`
             }
         }
     }
@@ -32,13 +34,14 @@ class Account extends Component{
             })
         } else {
         }
+
     }
     
     sendEmail = () => {
         const { email } = this.state;
         axios.get(`/api/send-email?to=${email.to}&from=${email.from}&subject=${email.subject}&text=${email.text}&html=${email.html}`)
         .then(res => console.log('email sending'))
-        .catch(err => console.loglog(err))
+        .catch(err => console.log(err))
     }
 
     render(){
